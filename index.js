9231bbb01cfb847a8bb320f0482973998aeabbb1
Heath Boone <hmboone1@gmail.com>
2023-07-05 19:13:19 -0500

    Adds mtfile.md

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+const employee = {
+    name: "Heath",
+    streetAdress: "Broadway",
+}
+
+function  updateEmployeeWithKeyAndValue(object, key, value){
+    const newobj = {...object}
+    newobj[key] = value;
+    return newobj;
+}
+
+function  destructivelyUpdateEmployeeWithKeyAndValue(object, key, value){
+    object[key] = value
+    return object
+}
+
+
+
+
+
+
+
+function  deleteFromEmployeeByKey(object, key){
+   const newobj = {...object}
+    delete newobj[key]
+    return newobj
+}
+
+// console.log(deleteFromEmployeeByKey(luisobj, "favoriteMovie"))
+// console.log(luisobj)
+function destructivelyDeleteFromEmployeeByKey(object, key){
+    delete object[key]
+    return object
+}
